05974a78ef2b27031909a86c3c527acdd1f0b39e
dakotawhite-code <wd53169@mail.harpercollege.edu>
2024-10-20 19:04:43 -0500

    done

--- src/main/java/com/csc/TriangleNumberCalculator.java
+++ src/main/java/com/csc/TriangleNumberCalculator.java
@@ -1,5 +1,33 @@
 package com.csc;
-
 public class TriangleNumberCalculator {
-  // Your code goes here!
+    public int value(int n) {
+        if (n == 1) {
+          return 1;
+        }
+      return n + value(n - 1);
+    }
+
+    public int add(int n, int m) {
+        return value(n) + value(m);
+    }
+
+    public int subtract(int n, int m) {
+        return value(n) - value(m);
+    }
+
+    public static void main(String[] args) {
+        TriangleNumberCalculator calculator = new TriangleNumberCalculator();
+        
+        System.out.println(calculator.value(1));
+        System.out.println(calculator.value(4));
+        System.out.println(calculator.value(9));
+        
+        System.out.println(calculator.add(1, 1));
+        System.out.println(calculator.add(3, 4));
+        System.out.println(calculator.add(8, 5));
+        
+        System.out.println(calculator.subtract(1, 1));
+        System.out.println(calculator.subtract(5, 6));
+        System.out.println(calculator.subtract(8, 2));
+    }
 }
--- src/test/java/com/csc/TestTriangleNumberCalculator.java
+++ src/test/java/com/csc/TestTriangleNumberCalculator.java
@@ -1,9 +1,10 @@
 package com.csc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TestTriangleNumberCalculator {
-
   TriangleNumberCalculator calculator;
 
   @BeforeEach
@@ -11,5 +12,32 @@ public class TestTriangleNumberCalculator {
     calculator = new TriangleNumberCalculator();
   }
 
-  // Your tests go here
+@Test
+  void testvalue1() {
+    assertEquals(1,calculator.value(1));
+  }
+  @Test
+  void testvalue4() {
+    assertEquals(10,calculator.value(4));
+  }
+
+  @Test
+  void testadd1n1() {
+    assertEquals(2,calculator.add(1,1));
+  }
+
+  @Test
+  void testadd4n5() {
+    assertEquals(25,calculator.add(4,5));
+  }
+
+  @Test
+  void testsub1n1() {
+    assertEquals(0,calculator.subtract(1,1));
+  }
+
+  @Test
+  void testsub8n9() {
+    assertEquals(-9,calculator.subtract(8,9));
+  }
 }
